ca74ea51216f12bae2fbf7c15d9b24be873e766e
Pyatin3pk1 <pyatin.aa1242@gmail.com>
2024-04-29 23:21:36 +0500

    test

--- src/component/Message/header/HeaderMes.jsx
+++ src/component/Message/header/HeaderMes.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useDispatch } from 'react-redux';
 import Logo from '../../../assets/logo.png';
+import { MdClose } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { signOut } from "firebase/auth";
 import { auth } from "../../../firebase";
@@ -10,11 +11,13 @@ import { useNavigate } from "react-router-dom";
 const HeaderMes = () => {
   const {currentUser} = useContext(AuthContext);
   const navigate = useNavigate();
+  const [isOpen, setOpen] = useState();
   return (
     <>
         <header className="header">
         <div className="container">
-            <nav className="menu">
+            <nav className={`menu ${isOpen ? "active" : ""}`}>
+            <label className="burger-user">{currentUser.displayName}</label>
             <ul className="menu-list">
                 <li className="menu-item">
                 <a >Заявления</a>
@@ -25,22 +28,20 @@ const HeaderMes = () => {
                 <li className="menu-item">
                 <a >Уведомления</a>
                 </li>
-                <li className="menu-item">
+                <li className="menu-item user">
                 <a >{currentUser.displayName}</a>
                     <ul>
                         <li><button onClick={() =>{ signOut(auth); navigate("/login")}}>Выход</button></li>
                     </ul>
                 </li>
             </ul>
-            <GiHamburgerMenu
-                size={20}
-                className="navbarIcon"
-            />
+            <button className="menu-button"
+                onClick={() => setOpen(!isOpen)}><GiHamburgerMenu/></button>
+            <button className="button-logaut" onClick={() =>{ signOut(auth); navigate("/login")}}>Выход</button>
             </nav>
         </div>
         </header>
     </>
-    
   );
 }
 
