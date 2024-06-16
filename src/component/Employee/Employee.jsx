bb89b6fa674dfb0499c90aea1466cf29a40c7b13
Pyatin3pk1 <pyatin.aa1242@gmail.com>
2024-06-17 02:19:00 +0500

    test

--- src/component/Employee/Employee.jsx
+++ src/component/Employee/Employee.jsx
@@ -10,18 +10,20 @@ import { collection, doc, onSnapshot, updateDoc, getDoc, deleteDoc, deleteField 
 import { db } from '../../firebase';
 import AddEmployee from './addEmployee/AddEmployee';
 import Delete from '../../assets/delete.png';
-import handleSelect from './handleSelect';
+import handleSelect from './handleSelect'; 
+import { useNavigate } from 'react-router-dom';
 
 const Employee = () => {
     const [addModel, setAddModel] = useState(false);
+    const navigate = useNavigate();
     const [searchText, setSearchText] = useState('');
     const { currentUser } = useContext(AuthContext);
     const { departments, fetchUsers } = useContext(UserListContext);
     const [employees, setEmployees] = useState([]);
     const { dispatch } = useContext(ChatContext);
-    const [filteredEmployees, setFilteredEmployees] = useState([]); 
+    const [filteredEmployees, setFilteredEmployees] = useState([]);
     const [roles, setRoles] = useState({});
-    const [selectedRole, setSelectedRole] = useState(''); 
+    const [selectedRole, setSelectedRole] = useState('');
 
     useEffect(() => {
         if (currentUser && currentUser.uid) {
@@ -43,12 +45,12 @@ const Employee = () => {
             const users = snapshot.docs.map(doc => ({
                 uid: doc.id,
                 ...doc.data(),
-                photoURL: doc.data().photoURL instanceof Blob 
-                    ? URL.createObjectURL(doc.data().photoURL) 
+                photoURL: doc.data().photoURL instanceof Blob
+                    ? URL.createObjectURL(doc.data().photoURL)
                     : doc.data().photoURL
             }));
             setEmployees(users);
-            filterEmployeesByRole(selectedRole, users); 
+            filterEmployeesByRole(selectedRole, users);
         });
 
         return () => unsubscribe();
@@ -65,7 +67,7 @@ const Employee = () => {
 
         return () => unsubscribe();
     }, []);
-    
+
     const handleSearch = (e) => {
         const searchText = e.target.value.toLowerCase();
         setSearchText(searchText);
@@ -73,68 +75,63 @@ const Employee = () => {
         const filteredResults = employees.filter(employee =>
             employee.displayName.toLowerCase().includes(searchText)
         );
-        filterEmployeesByRole(selectedRole, filteredResults); 
+        filterEmployeesByRole(selectedRole, filteredResults);
     };
 
     const handleSortByRole = (e) => {
         const selectedRole = e.target.value;
-        setSelectedRole(selectedRole); 
-        filterEmployeesByRole(selectedRole, employees); 
+        setSelectedRole(selectedRole);
+        filterEmployeesByRole(selectedRole, employees);
     };
 
     const filterEmployeesByRole = (role, employeesList) => {
-        const filteredEmployees = role === '' 
-            ? employeesList 
+        const filteredEmployees = role === ''
+            ? employeesList
             : employeesList.filter(employee => employee.role === role);
-        const filteredResults = searchText === '' 
-            ? filteredEmployees 
+        const filteredResults = searchText === ''
+            ? filteredEmployees
             : filteredEmployees.filter(employee =>
                 employee.displayName.toLowerCase().includes(searchText)
             );
-        setFilteredEmployees(filteredResults); 
-    };
-    const handleClick = (u) => {
-        handleSelect(u, navigate, currentUser, dispatch);
+        setFilteredEmployees(filteredResults);
     };
 
-    useEffect(() => {
-        if (employees.length > 0) {
-            filterEmployeesByRole(selectedRole, employees); 
-        }
-    }, [employees, selectedRole]);
-    
+    const handleClick = (u) => {
+        handleSelect(u, navigate, currentUser, dispatch); // Вызываем функцию handleSelect при клике на сотрудника
+    };
+
     const handleDeleteEmployee = async (employee) => {
         const confirmDelete = window.confirm(`Вы уверены, что хотите удалить пользователя ${employee.displayName}?`);
-    
+
         if (!confirmDelete) {
             return;
         }
-    
+
         try {
             await deleteDoc(doc(db, "users", employee.uid));
-    
+
             const combinedId = currentUser.uid > employee.uid
                 ? currentUser.uid + employee.uid
                 : employee.uid + currentUser.uid;
-    
+
             const chatDocRef = doc(db, "chats", combinedId);
             const chatDoc = await getDoc(chatDocRef);
-    
+
             if (chatDoc.exists()) {
                 await deleteDoc(chatDocRef);
-    
+
                 const currentUserChatRef = doc(db, "userChats", currentUser.uid);
                 const currentUserChatDoc = await getDoc(currentUserChatRef);
-    
+
                 if (currentUserChatDoc.exists()) {
                     await updateDoc(currentUserChatRef, {
                         [combinedId]: deleteField()
                     });
                 }
-    
+
                 const employeeChatRef = doc(db, "userChats", employee.uid);
                 const employeeChatDoc = await getDoc(employeeChatRef);
-    
+
                 if (employeeChatDoc.exists()) {
                     await updateDoc(employeeChatRef, {
                         [combinedId]: deleteField()
@@ -143,12 +140,13 @@ const Employee = () => {
             } else {
                 console.log(`Chat document does not exist for combined ID: ${combinedId}`);
             }
-    
+
             fetchUsers();
         } catch (err) {
             console.error(`Error deleting employee: ${err.message}`);
         }
-    };    
+    };
+
     const closeModal = () => {
         setAddModel(false);
         fetchUsers();
@@ -162,28 +160,28 @@ const Employee = () => {
                         <div className="search__moduls">
                             <div className="searchBar">
                                 <img src={Search} alt="search" />
-                                <input 
-                                    type="text" 
-                                    placeholder="Поиск" 
-                                    value={searchText} 
-                                    onChange={handleSearch} 
+                                <input
+                                    type="text"
+                                    placeholder="Поиск"
+                                    value={searchText}
+                                    onChange={handleSearch}
                                 />
                             </div>
-                            <img 
-                                src={addModel ? Minus : Plus} 
+                            <img
+                                src={addModel ? Minus : Plus}
                                 className='addUser'
-                                onClick={() => setAddModel((prev) => !prev)} 
+                                onClick={() => setAddModel((prev) => !prev)}
                             />
                         </div>
-                        <select onChange={handleSortByRole} value={selectedRole}> 
-                            <option value="">Выберите роль</option> 
+                        <select onChange={handleSortByRole} value={selectedRole}>
+                            <option value="">Выберите роль</option>
                             {Object.entries(roles).map(([roleId, roleName]) => (
                                 <option key={roleId} value={roleId}>{roleName}</option>
                             ))}
                         </select>
                     </div>
                     <div className="employeeConteiner">
-                        {filteredEmployees.length > 0 ? ( 
+                        {filteredEmployees.length > 0 ? (
                             filteredEmployees.map((employee) => {
                                 const avatarUrl = employee.photoURL ? employee.photoURL : Avatar;
                                 const birthdate = employee.birthdate ? new Date(employee.birthdate).toLocaleDateString() : '-';
@@ -198,7 +196,7 @@ const Employee = () => {
                                                 <span>Роль: {roles[employee.role] || 'Роль не задана'}</span>
                                             </div>
                                         </div>
-                                        <button className="deleteChat" onClick={() => handleDeleteEmployee(employee)}> 
+                                        <button className="deleteChat" onClick={() => handleDeleteEmployee(employee)}>
                                             <img src={Delete} alt="Delete"/>
                                         </button>
                                     </div>
@@ -214,7 +212,7 @@ const Employee = () => {
             </div>
             {addModel && <AddEmployee closeModal={closeModal} />}
         </div>
-    );    
+    );
 };
 
 export default Employee;
